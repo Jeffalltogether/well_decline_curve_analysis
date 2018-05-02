0f751c3ed0a3e4a55bc1521e9051df65d20dd51b
jeffalltogether <jeff.thatcher@gmail.com>
2018-05-02 18:36:16 -0500

    added copyright

--- wellAnalysis.py
+++ wellAnalysis.py
@@ -1,4 +1,24 @@
-### drilling info analysis
+'''
+Drilling info analysis
+
+This program reads well header data and production logs (e.g. exported from Drilling Info as .csv files) and
+walks the user through the genreation of decline curves for each well provided in the input data. Decine curves
+are fit with a the hyperbolic curve that is estimated using an iterative least squares method.
+
+Copyright 2018 Jeffrey E. Thatcher
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
 
 ### Boiler-plate imports and code
 import sys
